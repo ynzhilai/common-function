8cf190bfe7711ae3ce90a5fd981a12b4f8009c95
ynzhilai@163.com <ynzhilai@163.com>
2018-09-14 23:12:34 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ynzhi
+ * Date: 2018/9/14
+ * Time: 22:59
+ */
+
+$a = new \Ynzl\Comm\Aa();
+
+echo $a->cs();
---
+++ lib/comm/Aa.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ynzhi
+ * Date: 2018/9/14
+ * Time: 14:13
+ */
+
+namespace Ynzl\Comm;
+
+
+class Aa
+{
+  public function cs()
+  {
+    echo "成功了！！！";
+}
+}
